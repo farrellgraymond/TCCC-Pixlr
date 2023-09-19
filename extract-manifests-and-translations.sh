c90ce365f58c403a5fa807d6015500249cf6e241
Ryan Slobojan <straxus@gmail.com>
2023-09-19 00:27:34 -0400

    Comment typo fix seen during code review

--- extract-manifests-and-translations.sh
+++ extract-manifests-and-translations.sh
@@ -6,7 +6,7 @@ PIXLR_FILES_DIR="Pixlr Source Files"
 XARGS_SH_CMD="unzip -p '{}' manifest.json | jq . | tee '../${MANIFEST_DIR}/{}.manifest.json'"
 
 # This ended up being a bad idea when the manifests folder started being used
-# for other files nad purposes, and those files disappeared unexpectedly :/
+# for other files and purposes, and those files disappeared unexpectedly :/
 #echo "=== Ensuring directory [${MANIFEST_DIR}] is deleted and recreated..."
 #rm -rf ${MANIFEST_DIR}
 #mkdir -p ${MANIFEST_DIR}
